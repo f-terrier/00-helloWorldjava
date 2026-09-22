c50054ae01ab41ae4a81a4bdbd2866ee64a0432f
fterrier <fterrier@350028R-F115X02.lise.kreizenn.bzh>
2026-09-22 12:39:15 +0200

    exercice 4

--- src/pkg00/helloworld/HelloWorld.java
+++ src/pkg00/helloworld/HelloWorld.java
@@ -8,12 +8,30 @@ package pkg00.helloworld;
  *
  * @author fterrier
  */
+import java.util.Scanner;
+
 public class HelloWorld {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner sx = new Scanner(System.in);
+        Scanner sy = new Scanner(System.in);
+        int x, y;
+        System.out.print("Saisir x : ");
+        x = sx.nextInt();
+        System.out.print("Saisir y : ");
+        y = sy.nextInt();
+        
+        System.out.println("Avant permutation : x=" + x +" y="+ y);
+        
+        int tmp = x;
+        x = y;
+        y = tmp;
+        
+        System.out.println("Apres permutation : x=" + x +" y="+ y);
+        
         // TODO code application logic here
         System.out.println("Hello world\nyay");
     }
